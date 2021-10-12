e84726fd2255513de9001cbd91584d166f79d82b
Levi Lugato <levilugato@MacBook-Pro-de-Levi.local>
2021-10-12 11:45:56 -0300

    add ELBsv2, TargetGroups, AMIs, RDS

--- unused.py
+++ unused.py
@@ -82,8 +82,9 @@ td {
     for region in region_list:
         append('<h1 style="color:red"><center>'+region+'</center></h1>')
 
-        #EIP
         client = boto3.client('ec2', region_name=region)
+
+        #EIP
         response = client.describe_addresses()
         eips=[]
         for address in response['Addresses']:
@@ -94,9 +95,6 @@ td {
             for eip in eips:
                 append('<tr><td>EIP</td><td>'+eip+'</td></tr>')
             append('</table>')
-
-
-
 
         #Volumes
         response=client.describe_volumes()
@@ -115,7 +113,6 @@ td {
                 append('<tr><td>Volume </td><td>'+vol['VolumeId']+'</td><td>'+vol['VolumeType']+'</td><td>'+str(vol['VolumeSize'])+'GB</td></tr>')
         append('</table>')
 
-
         #Snapshots
         response = client.describe_snapshots(OwnerIds=[owner_id])
         snapshots=[]
@@ -130,13 +127,11 @@ td {
             append('</table>')
 
 
-
         #Securit Groups
         response = client.describe_security_groups()
         all_sec_groups = []
         for SecGrp in response['SecurityGroups']:
             all_sec_groups.append(SecGrp['GroupName'])
-
 
         sec_groups_in_use = []
         response = client.describe_instances(
@@ -158,17 +153,13 @@ td {
             if groups not in sec_groups_in_use:
                 unused_sec_groups.append(groups)
 
-
-
         if len(unused_sec_groups) > 0:
             append('<br><table><caption>Unused Security Groups</caption><th>Resource</th><th>Security Group Name</th>')
             for sg in unused_sec_groups:
                 append('<tr><td>Security Group</td><td>'+sg+'</td></tr>')
         append('</table>')
 
-
-
-        #ELB
+        #ELBv1
         client = boto3.client('elb', region_name=region)
         response = client.describe_load_balancers()
         elbs=[]
@@ -177,12 +168,143 @@ td {
                 elbs.append(ELB['LoadBalancerName'])
 
         if len(elbs) > 0:
-            append('<br><table><caption>Unused ELBs</caption><th>Resource</th><th>ELB Name</th>')
+            append('<br><table><caption>Unused Classic ELBs</caption><th>Resource</th><th>ELB Name</th>')
             for elb in elbs:
                 append('<tr><td>ELB</td><td>'+elb+'</td></tr>')
         append('</table>')
 
+        #Ami's
+        client = boto3.client('ec2', region_name=region)
+        
+        instances = client.describe_instances()
+        used_amis = []
+        for reservation in instances['Reservations']:
+            for instance in reservation['Instances']:
+                used_amis.append(instance['ImageId'])
+        
+        custom_images = client.describe_images(
+            Filters=[
+                   {
+                   'Name': 'state',
+                   'Values': [
+                       'available'
+                   ]
+               },
+            ],
+            Owners= ['self']
+        )
+        
+        custom_amis_list = []
 
+        for image in custom_images['Images']:
+            custom_amis_list.append(image['ImageId'])
+        
+        if len(custom_amis_list) > 0:
+            for custom_ami in custom_amis_list:
+                if custom_ami not in used_amis:
+                    append('<br><table><caption>Unused AMIs</caption><th>Resource</th><th>AMI Id/th>')
+                    for ami in custom_amis_list:
+                        append('<tr><td>AMI</td><td>'+ami+'</td></tr>')
+        append('</table>')
+            
+        #ELBv2
+        client = boto3.client('elbv2', region_name=region)
+        
+        response_targets = client.describe_target_groups()
+        response_lbs = client.describe_load_balancers()
+        
+        elbsv2_arns=[]
+        elbsv2_target_arns=[]
+        
+        elbsv2_unused=[]
+        elbsv2_unhealthy=[]
+        elbsv2_nolisteners=[]
+
+        #ELBv2 check Target groups
+        for tg in response_targets['TargetGroups']:
+                elbsv2_target_arns.append(tg['TargetGroupArn'])
+        
+        for target_group in elbsv2_target_arns:
+            response_healthy = client.describe_target_health(TargetGroupArn=target_group)
+            for target in response_healthy['TargetHealthDescriptions']:
+                if target['TargetHealth']['State'] == "unused" :
+                    elbsv2_unused.append(target_group)
+                elif target['TargetHealth']['State'] == "unhealthy" :
+                    elbsv2_unhealthy.append(target_group)
+
+        #ELBv2 check Load Balancers without Listeners
+        for ELB in response_lbs['LoadBalancers']:
+            elbsv2_arns.append(ELB['LoadBalancerArn'])
+        
+        for ELB in elbsv2_arns:
+            response_listeners = client.describe_listeners(LoadBalancerArn=ELB) 
+            if response_listeners['Listeners'] == []:
+                elbsv2_nolisteners.append(ELB)
+        
+        # Fill HTML ELBsv2
+        if len(elbsv2_unused) > 0:
+            append('<br><table><caption>Unused Target Groups</caption><th>Resource</th><th>ELB Name</th>')
+            for elbv2 in elbsv2_unused:
+                append('<tr><td>ELB</td><td>'+elbv2+'</td></tr>')
+        append('</table>')
+
+        if len(elbsv2_unhealthy) > 0:
+            append('<br><table><caption>Unhealthy Target Groups</caption><th>Resource</th><th>ELB Name</th>')
+            for elbv2 in elbsv2_unhealthy:
+                append('<tr><td>ELB</td><td>'+elbv2+'</td></tr>')
+        append('</table>')
+        
+        if len(elbsv2_nolisteners) > 0:
+            append('<br><table><caption>LoadBalancer with no Listeners</caption><th>Resource</th><th>ELB Name</th>')
+            for elbv2 in elbsv2_nolisteners:
+                append('<tr><td>ELB</td><td>'+elbv2+'</td></tr>')
+        append('</table>')
+
+        #RDS
+        client = boto3.client('rds', region_name=region)
+        response = client.describe_db_instances()
+        rds_instances = []
+        unused_rds_instances = []
+        
+        for rds in response['DBInstances']:
+            rds_instances.append(rds['DBInstanceIdentifier'])
+              #rds_instances(rds['DBInstanceIdentifier']) += rds['Endpoint']['Address']
+        
+        # cloudwatch (check last connections RDS)
+        client_cloudwatch = boto3.client('cloudwatch', region_name=region)
+
+        end_date = datetime.utcnow()
+        start_date = end_date - timedelta(days=30)
+
+        # Getting DatabaseConnections metrics for the last 30 days
+        for db in rds_instances:
+            connection_statistics = client_cloudwatch.get_metric_statistics(
+            Namespace='AWS/RDS',
+            MetricName='DatabaseConnections',
+            Dimensions=[
+                {
+                'Name': 'DBInstanceIdentifier',
+                'Value': db
+                },
+            ],
+            StartTime=start_date,
+            EndTime=end_date,
+            Period=86400,
+            Statistics=["Maximum"]
+            )
+
+            total_connection_count = sum(data_point['Maximum']
+                                 for data_point in connection_statistics['Datapoints'])
+
+            if total_connection_count == 0:
+                unused_rds_instances.append(db)
+
+        # Fill HTML RDS
+        if len(unused_rds_instances) > 0:
+            append('<br><table><caption>Unused RDS Instances (No connection last 30 days)</caption><th>Resource</th><th>RDS DB</th>')
+            for rds in unused_rds_instances:
+                append('<tr><td>RDS</td><td>'+rds+'</td></tr>')
+        append('</table>')
 
         #Autoscaling
         client = boto3.client('autoscaling', region_name=region)
